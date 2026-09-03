92804a5a058c1bd274e6e38aed75b22849fe9b1b
nandan166 <nandanabhi7899@gmail.com>
2026-09-03 12:59:02 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+	int num1,num2,sum=0;
+	printf("Enter Number 1 : ");
+	scanf("%d",&num1);
+	printf("Enter Number 2 : ");
+	scanf("%d",&num2);
+	sum = num1 + num2;
+	printf("Sum : %d",sum);
+}
